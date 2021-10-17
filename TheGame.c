612959bbf4fc84e668bdb152347dc104942fc3e0
Gabriel Claudino <gaclaudino.13@gmail.com>
2021-10-16 22:58:37 -0300

    criando as peças
    falta limitar para um total de 8 de cada numero

--- TheGame.c
+++ TheGame.c
@@ -1,9 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+#include "int_pilha.h"
+#include "int_fila.h"
+
+char nome_jogador1[21];
+char nome_jogador2[21];
+
+typedef struct{
+    int esquerda;
+    int direita;
+}tp_peca;
 
 int main(){
+    int i, num1, num2;
 
+    //Criando as peças.
+    tp_peca pecas[28];
+    for(i=0;i<28;i++){
+        pecas[i].esquerda = 0;
+        pecas[i].direita = 0;
+    }
+
+    srand(time(NULL));
+    for(i=0;i<28;i++){
+        num1 = rand() % 6;
+        num2 = rand() % 6;
+        pecas[i].esquerda = num1;
+        pecas[i].direita = num2;
+    }
     
+    for(i=0;i<28;i++){
+        printf("|%d|",pecas[i].esquerda );
+        printf("%d|", pecas[i].direita);        
+    }
+    printf("\n");
 
 return 0;
 }
+
+/*
+    //Para dois jogadores
+    printf("Nome do Jogador 1: ");
+    scanf(" %[\n]s", nome_jogador1);
+    printf("Nome do Jogador 2: ");
+    scanf(" %[\n]s", nome_jogador2);
+*/
---
+++ int_pilha.h
@@ -0,0 +1,109 @@
+#include <stdio.h>
+#define MAX 100
+
+typedef int 
+    tp_item;
+
+typedef struct{
+    int topo;           //inicialmente topo = -1
+    tp_item item[MAX];
+}tp_pilha;
+
+void inicializa_pilha(tp_pilha *p){
+    p->topo = -1;
+}
+
+int pilha_vazia(tp_pilha *p){       //Pilha vc está vazia? 
+    if (p->topo == -1) return 1;    //Sim, estou!
+    return 0;                       //Não estou.
+}
+
+int pilha_cheia(tp_pilha *p){           //Pilha vc está cheia?
+    if (p->topo == MAX -1) return 1;    //Sim, estou :/
+    return 0;                           //Não estou :)
+}
+
+int push (tp_pilha *p, tp_item e){      //push = EMPILHAR 
+    if (pilha_cheia(p)) return 0;       //verificar se esta cheia
+    p->topo++;                          //incrementa 1 ao topo
+    p->item[p->topo]=e;                 //colocar o "e" no topo da pilha
+    return 1;
+}
+
+int pop(tp_pilha *p, tp_item *e){        //pop = DESEMPILHAR; retorna para a main o "e".
+    if (pilha_vazia(p)) return 0;        //Verifica se esta cheia
+    *e=p->item[p->topo];                 //Coloca em "e" o item no topo
+    p->topo--;                           //Decresce 1 do valor do topo
+    return 1;
+}
+
+int top(tp_pilha *p, tp_item *e){       //top: mostra quem ta no topo sem desempilhar
+    if (pilha_vazia(p)) return 0;
+    *e=p->item[p->topo];
+    return 1;   
+}
+
+void imprime_pilha(tp_pilha p){         
+    tp_item e;
+    printf("Pilha:\n");
+    while (!pilha_vazia(&p)){
+        pop(&p,&e);
+        printf("%d ",e);
+    }
+}
+
+int altura_pilha(tp_pilha *p){
+    return p->topo+1;
+}
+
+/*
+Implementar uma função que receba uma pilha 
+como parâmetro e retire todos os
+elementos ímpares dessa pilha.
+*/
+int retira_impares(tp_pilha *p){
+    if (pilha_vazia(p)) return 0;
+    tp_pilha paux;
+    inicializa_pilha(&paux);
+    tp_item e;
+    while (!pilha_vazia(p)){
+        pop(p, &e);
+        if (e % 2 == 0)
+            push(&paux, e);
+    }
+    while(!pilha_vazia(&paux)){
+        pop(&paux, &e);
+        push(p, e);
+    }
+    return 1;
+}
+
+/*
+Implementar uma função que receba duas pilhas 
+como parâmetro e retorna 1 caso sejam
+idênticas ou zero caso contrário.
+*/
+int pilhas_iguais (tp_pilha p1, tp_pilha p2){
+    if (altura_pilha(&p1) != altura_pilha(&p2)) return 0;
+    tp_item e1, e2;
+    while (!pilha_vazia(&p1)){
+        pop(&p1, &e1);
+        pop(&p2, &e2);
+        if (e1 != e2) return 0;
+    }
+    return 1;  
+}
+
+void empilhar(tp_pilha *p1, tp_pilha *p2){
+    tp_pilha paux;
+    inicializa_pilha(&paux);
+    tp_item e;
+    while(!pilha_vazia(p2)){
+        pop(p2, &e);
+        push(&paux, e);
+    }
+    while (!pilha_vazia(&paux)){
+        pop(&paux, &e);
+        push(p1, e);
+    }
+}
